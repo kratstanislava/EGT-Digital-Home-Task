a0f476c1ccebbd8302c3d8fda0214a04f672dc77
kratstanislava <kratstanislava2000@gmail.com>
2025-02-11 22:31:25 +0200

    add settings for db connection

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER=os.getenv('DB_USER', 'username')
+DB_PASSWORD=os.getenv('DB_PASSWORD', 'password')
+DB_HOST=os.getenv('DB_HOST', '127.0.0.1')
+DB_PORT=os.getenv('DB_PORT', '5432')
